22477a6e05db9b946f5a7bf313a3d079d91a462d
aydreeihn <adriane@enhancesoft.com>
2020-10-16 12:16:17 -0500

    Add 2fa Email Template
    This commit adds a new Agent Template to the database using the latest database task. The template can be found here:
    
    Admin Panel | Settings | Agents | Templates | Two Factor Authentication Email

--- include/upgrader/streams/core/e7038ce9-8cd26958.task.php
+++ include/upgrader/streams/core/e7038ce9-8cd26958.task.php
@@ -55,6 +55,27 @@ class AddIndexMigration extends MigrationTask {
                 }
             }
         }
+
+        //2fa template addition:
+        foreach (array('email2fa-staff') as $type) {
+             $i18n = new Internationalization();
+             $tpl = $i18n->getTemplate("templates/page/{$type}.yaml");
+             if (!($page = $tpl->getData()))
+                 // No such template on disk
+                 continue;
+
+              if ($id = db_result(db_query('select id from '.PAGE_TABLE
+                     .' where `type`='.db_input($type))))
+                 // Already have a template for the content type
+                 continue;
+
+              $sql = 'INSERT INTO '.PAGE_TABLE.' SET type='.db_input($type)
+                 .', name='.db_input($page['name'])
+                 .', body='.db_input($page['body'])
+                 .', notes='.db_input($page['notes'])
+                 .', created=NOW(), updated=NOW(), isactive=1';
+             db_query($sql);
+         }
     }
 }
 return 'AddIndexMigration';
